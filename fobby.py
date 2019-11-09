7bc0b6f33f2b19a9e779cfe0b9686ece2525749a
madprogramer <themadprogramer@gmail.com>
2019-11-09 04:43:48 +0300

    Decided to revive Fobby
    Started as an initiative to generate GUI for YAML, not sure where we're going from here...

---
+++ fobby.py
@@ -0,0 +1,33 @@
+#Welp, time to repurpose this abandoned repo 
+#Switching from tkinter to pyqt 6 years later...
+
+#Main Goal for this stage: Link widgets to varying datatypes
+
+import os, os.path, random, re, pickle, atexit, sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class Fobby(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Fobby'
+        self.left = 300
+        self.top = 300
+        self.width = 250
+        self.height = 250
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        label = QLabel(self)
+        pixmap = QPixmap('fobby.gif')
+        label.setPixmap(pixmap)
+        self.resize(pixmap.width(),pixmap.height())
+        
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Fobby()
+    sys.exit(app.exec_())
